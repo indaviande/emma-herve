56a0489f82c9d55758095844dcecee180bb01a25
indaviande <vianney.bernet@gmail.com>
2020-09-24 18:13:05 +0200

    removing imagesLoaded

--- src/js/components/barba.js
+++ src/js/components/barba.js
@@ -1,5 +1,4 @@
 import barba from '@barba/core';
-import imagesLoaded from 'imagesloaded';
 import gsap from 'gsap';
 import runScripts from './animations';
 import menuScripts from './menu';
@@ -13,8 +12,7 @@ stickyScripts();
 
 const scroll2top = () => {
 	window.scrollTo({
-		top: 0,
-		behavior: 'smooth'
+		top: 0
 	});
 };
 
@@ -24,16 +22,13 @@ barba.init({
 			name: 'fadein',
 			once({ current, next, trigger }) {
 				return new Promise(resolve => {
-					const images = document.querySelectorAll(images);
-					imagesLoaded(images, () => {
-						const timeline = gsap.timeline({
-							onComplete() {
-								resolve();
-							}
-						});
-
-						timeline.set(next.container, { opacity: 0 }).to(next.container, { opacity: 1 });
+					const timeline = gsap.timeline({
+						onComplete() {
+							resolve();
+						}
 					});
+
+					timeline.set(next.container, { opacity: 0 }).to(next.container, { opacity: 1 });
 				});
 			},
 			leave({ current, next, trigger }) {
@@ -132,6 +127,63 @@ barba.init({
 						.to(next.container, { opacity: 1 })
 						.to('.bg-hand', { opacity: 1, x: '0' })
 						.to('.hp-questions li', { opacity: 1, stagger: 0.1 });
+				});
+			}
+		},
+		{
+			name: 'contact-transition',
+			to: { namespace: 'contact' },
+			once({ current, next, trigger }) {
+				return new Promise(resolve => {
+					const timeline = gsap.timeline({
+						onComplete() {
+							resolve();
+						}
+					});
+
+					timeline
+						.set(next.container, { opacity: 0 })
+						.set('.contact-shapes img', { opacity: 0 })
+						.to('footer', { y: '0' }, 0)
+						.to(next.container, { opacity: 1 })
+						.to('.contact-shapes img', { opacity: 1, stagger: 0.5 });
+				});
+			},
+			leave({ current, next, trigger }) {
+				return new Promise(resolve => {
+					const timeline = gsap.timeline({
+						onComplete() {
+							current.container.remove();
+							resolve();
+						}
+					});
+
+					timeline
+						.to('footer', { y: '100%' }, 0)
+						.to(current.container, { opacity: 0 })
+						.to(next.container, { opacity: 0 });
+				});
+			},
+			enter({ current, next, trigger }) {
+				return new Promise(resolve => {
+					scroll2top();
+
+					const timeline = gsap.timeline({
+						onComplete() {
+							runScripts();
+							menuScripts();
+							optimizations();
+							stickyScripts();
+							resolve();
+						}
+					});
+
+					timeline
+						.set(next.container, { opacity: 0 })
+						.set('.contact-shapes img', { opacity: 0 })
+						.to('footer', { y: '0' }, 0)
+						.to(next.container, { opacity: 1 })
+						.to('.contact-shapes img', { opacity: 1, stagger: 0.5 });
 				});
 			}
 		}
